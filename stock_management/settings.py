08ad6e038d397e981ab01fc6b16a0b8023eacd15
Sai Kumar <dagudusai@gmail.com>
2024-05-20 09:49:28 +0530

    static files copied

--- stock_management/settings.py
+++ stock_management/settings.py
@@ -147,7 +147,7 @@ SESSION_COOKIE_SECURE = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
